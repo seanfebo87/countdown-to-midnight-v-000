59dfb572f8ef33157e089592012bab9f0db02220
Sean Febo <seanfebo@techie.com>
2018-04-05 18:46:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,9 @@
 #write your code here
 
-def countdown(integer)
-  
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number = number - 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
